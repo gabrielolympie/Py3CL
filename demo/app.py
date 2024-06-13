Minor bug fix on intermitence calculation for collective buildings

demo/app.py
@@ -12,6 +12,7 @@ import uuid
 sys.path.append(".")
 from py3cl import DPE, DPEInput, abaques_configs
 from py3cl.utils import save_config
+
 pd.set_option("display.max_columns", 500)
 
 
@@ -406,7 +407,12 @@ def entangled_dropdown(valid_combinations, input_scheme, prefix=""):
     keys = [elt for elt in valid_combinations[0].keys() if elt in input_scheme]
 
     def reset_dropdown():
-        out = [gr.update(choices=list(set([elt[key] for elt in valid_combinations])), value=None) for key in keys]
+        out = [
+            gr.update(
+                choices=list(set([elt[key] for elt in valid_combinations])), value=None
+            )
+            for key in keys
+        ]
         return out
 
     def update_dropdown(*args):
@@ -414,7 +420,9 @@ def entangled_dropdown(valid_combinations, input_scheme, prefix=""):
         for key, arg in zip(keys, args):
             if arg:
                 valid = [elt for elt in valid if elt[key] == arg]
-        out = [gr.update(choices=list(set([elt[key] for elt in valid]))) for key in keys]
+        out = [
+            gr.update(choices=list(set([elt[key] for elt in valid]))) for key in keys
+        ]
         return out
 
     with gr.Group():
@@ -438,12 +446,16 @@ def entangled_dropdown(valid_combinations, input_scheme, prefix=""):
             )
 
         reset_button = gr.Button("Reset Group Selection", update_dropdown)
-        reset_button.click(reset_dropdown, outputs=[dropdowns[prefix + k] for k in keys])
+        reset_button.click(
+            reset_dropdown, outputs=[dropdowns[prefix + k] for k in keys]
+        )
 
     return dropdowns
 
 
-def create_processor(processor, identifiants=None, identifiants_adjacents=None, prefix=""):
+def create_processor(
+    processor, identifiants=None, identifiants_adjacents=None, prefix=""
+):
     valid_combinations = processor.valid_cat_combinations
     key_characteristics = processor.key_characteristics
     input_scheme = processor.input_scheme
@@ -474,7 +486,9 @@ def create_processor(processor, identifiants=None, identifiants_adjacents=None, 
             key_id = prefix + key
 
             if type(characteristic) == str and characteristic == "any":
-                if not (key in ["parois", "vitrages", "ponts_thermiques", "installations"]):
+                if not (
+                    key in ["parois", "vitrages", "ponts_thermiques", "installations"]
+                ):
                     if key == "identifiant":
                         input_field = gr.Dropdown(
                             choices=identifiants,
@@ -513,7 +527,11 @@ def create_processor(processor, identifiants=None, identifiants_adjacents=None, 
                     key=key_id,
                     value=" ",
                 )
-            elif isinstance(characteristic, dict) and "min" in characteristic and "max" in characteristic:
+            elif (
+                isinstance(characteristic, dict)
+                and "min" in characteristic
+                and "max" in characteristic
+            ):
                 input_field = gr.Slider(
                     minimum=0,
                     maximum=characteristic["max"],
@@ -522,9 +540,13 @@ def create_processor(processor, identifiants=None, identifiants_adjacents=None, 
                     info=input_descriptors[key]["description"],
                     key=key_id,
                 )
-            elif isinstance(characteristic, list) or isinstance(characteristic, np.ndarray):
+            elif isinstance(characteristic, list) or isinstance(
+                characteristic, np.ndarray
+            ):
                 char = [
-                    elt for elt in characteristic if ((elt is not None) and (elt != "NULL") and (str(elt) != "nan"))
+                    elt
+                    for elt in characteristic
+                    if ((elt is not None) and (elt != "NULL") and (str(elt) != "nan"))
                 ] + ["Unknown or Empty"]
                 input_field = gr.Dropdown(
                     choices=char,
@@ -543,7 +565,9 @@ def create_processor(processor, identifiants=None, identifiants_adjacents=None, 
 
 
 def get_demo(dpe):
-    with gr.Blocks(theme="freddyaboulton/dracula_revamped", title="Py3CL by Renovly") as demo:
+    with gr.Blocks(
+        theme="freddyaboulton/dracula_revamped", title="Py3CL by Renovly"
+    ) as demo:
         with gr.Tab(label="General Informations"):
             base_inputs = create_processor(dpe)
 
@@ -584,12 +608,20 @@ def get_demo(dpe):
         with gr.Tab(label="ECS"):
             for i in range(3):
                 with gr.Accordion(label=f"ECS {i}", open=False):
-                    base_inputs.update(create_processor(dpe.ecs_processor, identifiants=ecs, prefix=f"ecs_{i}-"))
+                    base_inputs.update(
+                        create_processor(
+                            dpe.ecs_processor, identifiants=ecs, prefix=f"ecs_{i}-"
+                        )
+                    )
 
         with gr.Tab(label="Climatisation"):
             for i in range(1):
                 with gr.Accordion(label=f"Climatisation {i}", open=False):
-                    base_inputs.update(create_processor(dpe.clim_processor, identifiants=clims, prefix=f"clim_{i}-"))
+                    base_inputs.update(
+                        create_processor(
+                            dpe.clim_processor, identifiants=clims, prefix=f"clim_{i}-"
+                        )
+                    )
 
         with gr.Tab(label="Chauffage"):
             for i in range(3):
@@ -611,19 +643,45 @@ def get_demo(dpe):
                 elif val == " ":
                     dico[k] = None
 
-            list_parois = list(set([elt.split("-")[0] for elt in dico.keys() if "paroi_" in elt]))
-            list_vitrages = list(set([elt.split("-")[0] for elt in dico.keys() if "vitrage_" in elt]))
-            list_ponts_thermiques = list(set([elt.split("-")[0] for elt in dico.keys() if "pont_thermique_" in elt]))
-            list_ecs = list(set([elt.split("-")[0] for elt in dico.keys() if "ecs_" in elt]))
-            list_clims = list(set([elt.split("-")[0] for elt in dico.keys() if "clim_" in elt]))
-            list_chauffages = list(set([elt.split("-")[0] for elt in dico.keys() if "chauffage_" in elt]))
+            list_parois = list(
+                set([elt.split("-")[0] for elt in dico.keys() if "paroi_" in elt])
+            )
+            list_vitrages = list(
+                set([elt.split("-")[0] for elt in dico.keys() if "vitrage_" in elt])
+            )
+            list_ponts_thermiques = list(
+                set(
+                    [
+                        elt.split("-")[0]
+                        for elt in dico.keys()
+                        if "pont_thermique_" in elt
+                    ]
+                )
+            )
+            list_ecs = list(
+                set([elt.split("-")[0] for elt in dico.keys() if "ecs_" in elt])
+            )
+            list_clims = list(
+                set([elt.split("-")[0] for elt in dico.keys() if "clim_" in elt])
+            )
+            list_chauffages = list(
+                set([elt.split("-")[0] for elt in dico.keys() if "chauffage_" in elt])
+            )
             list_installations = list_ecs + list_clims + list_chauffages
 
-            special = list_parois + list_vitrages + list_ponts_thermiques + list_installations
+            special = (
+                list_parois + list_vitrages + list_ponts_thermiques + list_installations
+            )
             for elt in special:
                 print(elt, dico[elt + "-identifiant"])
-            used_special = [elt for elt in special if dico[elt + "-identifiant"] != "Unknown or Empty"]
-            base = {k: v for k, v in dico.items() if not any([elt in k for elt in special])}
+            used_special = [
+                elt
+                for elt in special
+                if dico[elt + "-identifiant"] != "Unknown or Empty"
+            ]
+            base = {
+                k: v for k, v in dico.items() if not any([elt in k for elt in special])
+            }
 
             (
                 base["parois"],
@@ -639,30 +697,35 @@ def get_demo(dpe):
                 elif "vitrage" in s:
                     base["vitrages"].update({dico[s + "-identifiant"]: special_dico})
                 elif "pont_thermique" in s:
-                    base["ponts_thermiques"].update({dico[s + "-identifiant"]: special_dico})
+                    base["ponts_thermiques"].update(
+                        {dico[s + "-identifiant"]: special_dico}
+                    )
                 else:
-                    base["installations"].update({dico[s + "-identifiant"]: special_dico})
-            id_run=uuid.uuid4()
+                    base["installations"].update(
+                        {dico[s + "-identifiant"]: special_dico}
+                    )
+            id_run = uuid.uuid4()
             try:
                 dpe_input = DPEInput(**base)
                 result = dpe.forward(dpe_input)
-                save_config(result, 'results/config.yaml')
+                save_config(result, "results/config.yaml")
                 return result
             except Exception as e:
                 base["error"] = str(e)
-                save_config(base, 'results/config.yaml')
+                save_config(base, "results/config.yaml")
                 return base
-            
+
         # def download_json():
         #     return data
-
 
         with gr.Tab(label="Results"):
             with gr.Row():
                 with gr.Column(scale=3):
                     button = gr.Button("Compute")
                 with gr.Column(scale=1):
-                    download = gr.DownloadButton("Download", value="results/config.yaml")
+                    download = gr.DownloadButton(
+                        "Download", value="results/config.yaml"
+                    )
             output = gr.JSON()
             button.click(
                 get_json,

demo/app_streamlit_deprecated.py
@@ -31,7 +31,9 @@ clims = [f"clim_{i}" for i in range(1)]
 chauffages = [f"chauffage_{i}" for i in range(3)]
 
 
-def recursive_selectbox(valid, keys, select={}, prefix="", cols=None, col_count=0, container=None):
+def recursive_selectbox(
+    valid, keys, select={}, prefix="", cols=None, col_count=0, container=None
+):
     if not keys:
         return select, col_count
     k = keys[0]
@@ -63,7 +65,9 @@ def recursive_selectbox(valid, keys, select={}, prefix="", cols=None, col_count=
     return select, col_count
 
 
-def nested_selectbox(valid_cat_combinations, prefix="", cols=None, col_count=0, container=None):
+def nested_selectbox(
+    valid_cat_combinations, prefix="", cols=None, col_count=0, container=None
+):
     keys = valid_cat_combinations["keys"]
     return recursive_selectbox(
         valid_cat_combinations["combinations"],
@@ -75,12 +79,16 @@ def nested_selectbox(valid_cat_combinations, prefix="", cols=None, col_count=0, 
     )
 
 
-def build_processor(processor, identifiants=None, identifiants_adjacents=None, prefix="", col=4):
+def build_processor(
+    processor, identifiants=None, identifiants_adjacents=None, prefix="", col=4
+):
     input_scheme = processor.input_scheme
     key_characteristics = processor.key_characteristics
     valid_cat_combinations = processor.valid_cat_combinations
 
-    combinations_keys = {k for cat_comb in valid_cat_combinations.values() for k in cat_comb["keys"]}
+    combinations_keys = {
+        k for cat_comb in valid_cat_combinations.values() for k in cat_comb["keys"]
+    }
     inputs = {}
 
     cols = st.columns(col)
@@ -93,28 +101,42 @@ def build_processor(processor, identifiants=None, identifiants_adjacents=None, p
                 with cols[col_count % col]:
                     if characteristic == "any":
                         if key == "identifiant":
-                            input_field = st.selectbox(label=key, options=identifiants, index=None, key=key_id)
+                            input_field = st.selectbox(
+                                label=key, options=identifiants, index=None, key=key_id
+                            )
                         elif key == "identifiant_adjacents":
-                            input_field = st.multiselect(label=key, options=identifiants_adjacents, key=key_id)
+                            input_field = st.multiselect(
+                                label=key, options=identifiants_adjacents, key=key_id
+                            )
                         elif "is_" in key:
-                            input_field = st.selectbox(label=key, options=[True, False], index=1, key=key_id)
+                            input_field = st.selectbox(
+                                label=key, options=[True, False], index=1, key=key_id
+                            )
                         else:
                             input_field = st.text_input(label=key, value="", key=key_id)
                     elif characteristic == "float":
                         input_field = st.number_input(label=key, key=key_id, value=None)
-                    elif isinstance(characteristic, dict) and "min" in characteristic and "max" in characteristic:
+                    elif (
+                        isinstance(characteristic, dict)
+                        and "min" in characteristic
+                        and "max" in characteristic
+                    ):
                         input_field = st.number_input(
                             label=key + f" ({0}, {int(characteristic['max'])})",
                             key=key_id,
                             value=None,
                         )
                     elif isinstance(characteristic, (list, np.ndarray)):
-                        char = [elt for elt in characteristic if elt and elt != "Unknown or Empty"] + [
-                            "Unknown or Empty"
-                        ]
+                        char = [
+                            elt
+                            for elt in characteristic
+                            if elt and elt != "Unknown or Empty"
+                        ] + ["Unknown or Empty"]
                         input_field = st.selectbox(label=key, options=char, key=key_id)
                     else:
-                        input_field = st.text(label="Unsupported type for " + key, key=key_id)
+                        input_field = st.text(
+                            label="Unsupported type for " + key, key=key_id
+                        )
                     inputs[key] = input_field
                     col_count += 1
 
@@ -139,13 +161,25 @@ def build_json(
     inputs = {}
     inputs.update(inputs_general)
 
-    inputs["parois"] = {elt["identifiant"]: elt for elt in inputs_parois if elt["identifiant"]}
-    inputs["vitrages"] = {elt["identifiant"]: elt for elt in inputs_vitrages if elt["identifiant"]}
-    inputs["ponts_thermiques"] = {elt["identifiant"]: elt for elt in inputs_ponts_thermiques if elt["identifiant"]}
+    inputs["parois"] = {
+        elt["identifiant"]: elt for elt in inputs_parois if elt["identifiant"]
+    }
+    inputs["vitrages"] = {
+        elt["identifiant"]: elt for elt in inputs_vitrages if elt["identifiant"]
+    }
+    inputs["ponts_thermiques"] = {
+        elt["identifiant"]: elt for elt in inputs_ponts_thermiques if elt["identifiant"]
+    }
 
-    installations = {elt["identifiant"]: elt for elt in inputs_ecs if elt["identifiant"]}
-    installations.update({elt["identifiant"]: elt for elt in inputs_clims if elt["identifiant"]})
-    installations.update({elt["identifiant"]: elt for elt in inputs_chauffages if elt["identifiant"]})
+    installations = {
+        elt["identifiant"]: elt for elt in inputs_ecs if elt["identifiant"]
+    }
+    installations.update(
+        {elt["identifiant"]: elt for elt in inputs_clims if elt["identifiant"]}
+    )
+    installations.update(
+        {elt["identifiant"]: elt for elt in inputs_chauffages if elt["identifiant"]}
+    )
     inputs["installations"] = installations
     return inputs
 
@@ -166,7 +200,9 @@ if __name__ == "__main__":
     ]
     tabs = st.tabs(tab_names)
 
-    inputs_general = build_processor(st.session_state.processor) if "Général" in tab_names else {}
+    inputs_general = (
+        build_processor(st.session_state.processor) if "Général" in tab_names else {}
+    )
 
     inputs_parois = []
     with tabs[1]:

py3cl/libs/climatisation.py
@@ -86,7 +86,11 @@ class Climatisation(BaseProcessor):
             np.ndarray: Array of Rbth_j values.
         """
         Rbth_j_num = dpe["Ai_frj"] + dpe["Asj"] * (dpe["Ai_frj"] > 0)
-        Rbth_j_den = dpe["GV"] * (dpe["Textmoy_clim_j"] - dpe["Tint_froids"]) * dpe["Nref_froids_j"]
+        Rbth_j_den = (
+            dpe["GV"]
+            * (dpe["Textmoy_clim_j"] - dpe["Tint_froids"])
+            * dpe["Nref_froids_j"]
+        )
         Rbth_j = vectorized_safe_divide(Rbth_j_num, Rbth_j_den)
         Rbth_j[Rbth_j < 0.5] = 0
         return Rbth_j
@@ -124,7 +128,11 @@ class Climatisation(BaseProcessor):
         a = 1 + c_in / (dpe["GV"] * 3600 * 15)
         futj = np.array(
             [
-                (safe_divide(a, 1 + a) if elt == 1 else safe_divide(1 - elt ** (-a), 1 - elt ** (-a - 1)))
+                (
+                    safe_divide(a, 1 + a)
+                    if elt == 1
+                    else safe_divide(1 - elt ** (-a), 1 - elt ** (-a - 1))
+                )
                 for elt in rbth_j
             ]
         )
@@ -154,7 +162,12 @@ class Climatisation(BaseProcessor):
         clim["futj"] = futj
 
         bfroids_term1 = (dpe["Ai_frj"] + dpe["Asj"] * (dpe["Ai_frj"] > 0)) / 1000
-        bfroids_term2 = (futj * dpe["GV"] * (dpe["Tint_froids"] - dpe["Textmoy_clim_j"]) * dpe["Nref_froids_j"]) / 1000
+        bfroids_term2 = (
+            futj
+            * dpe["GV"]
+            * (dpe["Tint_froids"] - dpe["Textmoy_clim_j"])
+            * dpe["Nref_froids_j"]
+        ) / 1000
         clim["Bfrj"] = bfroids_term1 - bfroids_term2
 
         clim["EER"] = self.abaques["seer_clim"](
@@ -168,7 +181,9 @@ class Climatisation(BaseProcessor):
         clim["SEER"] = 0.95 * clim["EER"]
 
         clim["Cfr"] = 0.9 * clim["Bfrj"] / clim["SEER"]
-        clim["Cfr"] = clim["Cfr"] * safe_divide(clim["surface_refroidie"], dpe["surface_habitable"])
+        clim["Cfr"] = clim["Cfr"] * safe_divide(
+            clim["surface_refroidie"], dpe["surface_habitable"]
+        )
 
         if "Electricité" in clim["type_energie"]:
             clim["ratio_primaire_finale"] = 2.3

py3cl/libs/ouvrants.py
@@ -308,8 +308,15 @@ class Vitrage(BaseProcessor):
             float: The coefficient of reduction of loss (b).
         """
         exterior_type = vitrage["exterior_type_or_local_non_chauffe"]
-        if exterior_type in self.abaques["coef_reduction_deperdition_exterieur"].key_characteristics["aiu_aue"]:
-            return self.abaques["coef_reduction_deperdition_exterieur"]({"aiu_aue": exterior_type}, "valeur")
+        if (
+            exterior_type
+            in self.abaques["coef_reduction_deperdition_exterieur"].key_characteristics[
+                "aiu_aue"
+            ]
+        ):
+            return self.abaques["coef_reduction_deperdition_exterieur"](
+                {"aiu_aue": exterior_type}, "valeur"
+            )
         elif exterior_type == "Véranda":
             return self.abaques["coef_reduction_veranda"](
                 {
@@ -353,7 +360,9 @@ class Vitrage(BaseProcessor):
         """
         if vitrage["type_vitrage"] == "Simple Vitrage":
             return 5.8
-        orientation = "Horizontale" if vitrage["orientation"] == "Horizontal" else "Verticale"
+        orientation = (
+            "Horizontale" if vitrage["orientation"] == "Horizontal" else "Verticale"
+        )
         return self.abaques["ug_vitrage"](
             {
                 "type_vitrage": vitrage["type_vitrage"],
@@ -422,7 +431,10 @@ class Vitrage(BaseProcessor):
         """
         vitrage["type_vitrage_fs"] = vitrage["type_vitrage"]
         if vitrage["traitement_vitrage"] != "Non Traités":
-            if "Double" in vitrage["type_vitrage"] or "Triple" in vitrage["type_vitrage"]:
+            if (
+                "Double" in vitrage["type_vitrage"]
+                or "Triple" in vitrage["type_vitrage"]
+            ):
                 vitrage["type_vitrage_fs"] = vitrage["type_vitrage"] + " VIR"
 
         vitrage["type_baie_fs"] = menuiserie2baie[vitrage["type_menuiserie"]]
@@ -447,8 +459,16 @@ class Vitrage(BaseProcessor):
         Returns:
             np.ndarray: An array of orientation factors for each month.
         """
-        orientation = "Horizontal" if vitrage["orientation"] == "Horizontal" else vitrage["orientation"]
-        inclinaison = "Unknown or Empty" if vitrage["orientation"] == "Horizontal" else vitrage["inclinaison"]
+        orientation = (
+            "Horizontal"
+            if vitrage["orientation"] == "Horizontal"
+            else vitrage["orientation"]
+        )
+        inclinaison = (
+            "Unknown or Empty"
+            if vitrage["orientation"] == "Horizontal"
+            else vitrage["inclinaison"]
+        )
         return np.array(
             [
                 self.abaques["coefficient_orientation"](
@@ -538,4 +558,9 @@ class Vitrage(BaseProcessor):
         Returns:
             np.ndarray: An array of solar heat gains for each month.
         """
-        return vitrage["surface_vitrage"] * vitrage["facteur_solaire"] * vitrage["Fe"] * vitrage["c1j"]
+        return (
+            vitrage["surface_vitrage"]
+            * vitrage["facteur_solaire"]
+            * vitrage["Fe"]
+            * vitrage["c1j"]
+        )